71964144fa20f00054fc6b63122eede9e3bdb137
Олег Мальцев <oyumaltsev@sberbank.ru>
2026-04-20 21:00:14 +0300

    test: add Queue test

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,19 +1,34 @@
-// main function for the back-tester app
-// please, keep it minimalistic
-
-#include "common/BasicTypes.hpp"
-
+#include "common/SpscQueue.hpp"
+#include "common/MarketDataEvent.hpp"
+#include <thread>
 #include <iostream>
 
 using namespace cmf;
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] const char *argv[]) {
-  try {
-    std::cout << "Hell! Oh, world!" << std::endl;
-  } catch (std::exception &ex) {
-    std::cerr << "Back-tester threw an exception: " << ex.what() << std::endl;
-    return 1;
-  }
+int main() {
+  SpscQueue<MarketDataEvent, 1024> q;
+  bool done = false;
+  std::size_t count = 0;
 
+  // Consumer thread
+  std::thread consumer([&] {
+      MarketDataEvent e;
+      while (!done || count < 100) {
+          if (q.try_pop(e)) {  // или pop(e) с блокировкой
+              count++;
+          }
+      }
+      std::cout << "Consumer received: " << count << "\n";
+  });
+
+  // Producer thread
+  for (std::size_t i = 0; i < 100; ++i) {
+    MarketDataEvent e{};
+    e.ts_recv = i * 1000;
+    q.push(e);
+  }
+  done = true;
+
+  consumer.join();
   return 0;
 }
