aab8daa450b112e9e2ba262104983fcab4624bbf
polo7 <lesechko@gmail.com>
2026-05-05 02:36:35 +0300

    Read file line by line

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,15 +3,45 @@
 
 #include "common/BasicTypes.hpp"
 
+#include <fstream>
 #include <iostream>
+#include <cstddef>
+#include <string>
 
 using namespace cmf;
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] const char *argv[])
+int main(int argc, const char *argv[])
 {
     try
     {
-        std::cout << "Hell! Oh, world!" << std::endl;
+        if (argc != 2)
+        {
+            std::cerr << "Usage: back-tester <input_file_path>" << std::endl;
+            return 1;
+        }
+
+        const std::string inputFilePath{argv[1]};
+        std::ifstream inputFile(inputFilePath);
+        if (!inputFile.is_open())
+        {
+            std::cerr << "Failed to open file: " << inputFilePath << std::endl;
+            return 1;
+        }
+
+        std::string line;
+        std::size_t totalLines = 0;
+        while (std::getline(inputFile, line))
+        {
+            ++totalLines;
+        }
+
+        if (!inputFile.eof())
+        {
+            std::cerr << "Error while reading file: " << inputFilePath << std::endl;
+            return 1;
+        }
+
+        std::cout << "Total lines: " << totalLines << std::endl;
     }
     catch (std::exception &ex)
     {
